710af6955cd610b55796a9d21e3f91ee6f8f20b7
ShadowLp174 <samuel.moravec@yahoo.com>
2022-08-27 15:42:41 +0200

    multi-channel-support

--- Revoice.js
+++ Revoice.js
@@ -3,6 +3,91 @@ const Signaling = require("./Signaling.js");
 const EventEmitter = require("events");
 const { Device, useSdesMid, RTCRtpCodecParameters } = require("msc-node");
 
+class VoiceConnection {
+  constructor(channelId, voice, opts) {
+    this.voice = voice;
+    this.channelId = channelId;
+
+    this.device = opts.device;
+    this.signaling = opts.signaling;
+    this.setupSignaling();
+    this.signaling.connect(channelId);
+
+    this.media = null;
+
+    this.eventemitter = new EventEmitter();
+  }
+  on(event, cb) {
+    return this.eventemitter.on(event, cb);
+  }
+  once(event, cb) {
+    return this.eventemitter.once(event, cb);
+  }
+  emit(event, data) {
+    return this.eventemitter.emit(event, data);
+  }
+
+  updateState(state) {
+    this.state = state;
+    this.emit("state", state);
+  }
+
+  setupSignaling() {
+    const signaling = this.signaling;
+    signaling.on("token", console.log);
+    signaling.on("authenticate", (data) => {
+      this.device.load({ routerRtpCapabilities: data.data.rtpCapabilities });
+    });
+    signaling.on("initTransports", (data) => {
+      this.initTransports(data);
+    });
+  }
+  initTransports(data) {
+    const sendTransport = this.device.createSendTransport({...data.data.sendTransport});
+    this.sendTransport = sendTransport;
+    sendTransport.on("connect", ({ dtlsParameters }, callback) => {
+      this.signaling.connectTransport(sendTransport.id, dtlsParameters).then(callback);
+    });
+    sendTransport.on("produce", (parameters, callback) => {
+      this.signaling.startProduce("audio", parameters.rtpParameters).then((cid) => {
+        callback({ cid });
+      });
+    });
+
+    this.updateState(Revoice.State.IDLE);
+    this.emit("join");
+  }
+  async play(media) {
+    this.updateState(((!media.isMediaPlayer) ? Revoice.State.UNKNOWN : Revoice.State.BUFFERING));
+    media.on("finish", () => {
+      this.updateState(Revoice.State.IDLE);
+    });
+    media.on("buffer", () => {
+      this.updateState(Revoice.State.BUFFERING);
+    });
+    media.on("start", () => {
+      this.updateState(Revoice.State.PLAYING);
+    });
+    media.on("pause", () => {
+      this.updateState(Revoice.State.PAUSED);
+    });
+    this.media = media;
+    const track = media.track;
+    return await this.sendTransport.produce({ track: track, appData: { type: "audio" } }); // rtpProducer
+  }
+  disconnect() {
+    this.signaling.disconnect();
+    this.sendTransport.close();
+    this.sendTransport = undefined;
+    this.device = Revoice.createDevice();
+  }
+  leave() {
+    this.disconnect();
+    if (this.media) this.media.disconnect();
+    this.emit("leave");
+  }
+}
+
 class Revoice {
   static createDevice() {
     return new Device({
@@ -32,15 +117,22 @@ class Revoice {
     JOINING: "joining", // join process active
     UNKNOWN: "unknown" // online but a Media instance is used to play audio
   }
+  static Error = {
+    ALREADY_CONNECTED: "acon", // joining failed because already connected to a voice channel in this server
+    NOT_A_VC: "novc", // joining failed because the bot is already connected to the channel
+    VC_ERROR: "vce", // there was an error fetching data about the voice channel
+  }
   constructor(token) {
     this.api = new API({ authentication: { revolt: token }});
+    this.signals = new Map();
     this.signaling = new Signaling(this.api);
-    this.setupSignaling();
 
     this.eventemitter = new EventEmitter();
 
-    this.device = Revoice.createDevice();
-    this.media = null;
+    this.transports = new Map();
+    this.devices = new Map(); // list of devices by server id
+    this.connected = []; // list of channels the bot is connected to
+    this.connections = new Map();
 
     this.state = Revoice.State.OFFLINE;
 
@@ -49,16 +141,6 @@ class Revoice {
   updateState(state) {
     this.state = state;
     this.emit("state", state);
-  }
-  setupSignaling() {
-    const signaling = this.signaling
-    signaling.on("token", console.log);
-    signaling.on("authenticate", (data) => {
-      this.device.load({ routerRtpCapabilities: data.data.rtpCapabilities });
-    });
-    signaling.on("initTransports", (data) => {
-      this.initTransports(data);
-    });
   }
   on(event, cb) {
     return this.eventemitter.on(event, cb);
@@ -70,53 +152,32 @@ class Revoice {
     return this.eventemitter.emit(event, data);
   }
 
-  disconnect() {
-    this.signaling.disconnect();
-    this.sendTransport.close();
-    this.sendTransport = undefined;
-    this.device = Revoice.createDevice();
-  }
   join(channelId) {
-    this.updateState(Revoice.State.JOINING);
-    this.signaling.connect(channelId);
-  }
-  leave() {
-    this.disconnect();
-    if (this.media) this.media.disconnect();
-    this.emit("leave");
-  }
-  async initTransports(data) {
-    const sendTransport = this.device.createSendTransport({...data.data.sendTransport});
-    this.sendTransport = sendTransport;
-    sendTransport.on("connect", ({ dtlsParameters }, callback) => {
-      this.signaling.connectTransport(sendTransport.id, dtlsParameters).then(callback);
-    });
-    sendTransport.on("produce", (parameters, callback) => {
-      this.signaling.startProduce("audio", parameters.rtpParameters).then((id) => {
-        callback({ id });
+    return new Promise((res, rej) => {
+      this.api.get("/channels/" + channelId).then(data => {
+        if (data.channel_type != "VoiceChannel") return rej(Revoice.Error.NOT_A_VC);
+        if (this.devices.has(channelId)) {
+          return rej(Revoice.Error.ALREADY_CONNECTED);
+        }
+
+        const signaling = new Signaling(this.api);
+        const device = Revoice.createDevice();
+
+        const connection = new VoiceConnection(channelId, this, {
+          signaling: signaling,
+          device: device
+        });
+        connection.updateState(Revoice.State.JOINING);
+        this.connections.set(channelId, connection);
+        res(connection);
+      }).catch((e) => {
+        console.log(e);
+        rej(Revoice.Error.VC_ERROR);
       });
     });
-
-    this.updateState(Revoice.State.IDLE);
-    this.emit("join");
   }
-  async play(media) {
-    this.updateState(((!media.isMediaPlayer) ? Revoice.State.UNKNOWN : Revoice.State.BUFFERING));
-    this.media = media;
-    this.media.on("finish", () => {
-      this.updateState(Revoice.State.IDLE);
-    });
-    this.media.on("buffer", () => {
-      this.updateState(Revoice.State.BUFFERING);
-    });
-    this.media.on("start", () => {
-      this.updateState(Revoice.State.PLAYING);
-    });
-    this.media.on("pause", () => {
-      this.updateState(Revoice.State.PAUSED);
-    });
-    const track = media.track;
-    return await this.sendTransport.produce({ track: track, appData: { type: "audio" } }); // rtpProducer
+  getVoiceConnection(channelId) {
+    return this.connections.get(channelId);
   }
 }
 
--- test.js
+++ test.js
@@ -34,16 +34,31 @@ client.on("ready", () => {
 
 voice.on("state", (s) => {
   console.log(s);
-})
+});
 
-let media = new MediaPlayer(true);
+// Command pattern: prefix+command channelId input
+
+const media = new Map();
+var currPlayerPort = -1;
 client.on("message", (message) => {
   if (message.content.toLowerCase().startsWith(prefix + commands[0])) {
     const args = message.content.split(" ");
-    voice.join(args[1]);
+    voice.join(args[1]).then(connection => {
+      console.log(connection);
+      connection.on("state", (s) => {
+        console.log(s);
+      })
+    }).catch(e => {
+      console.log(e);
+    });
   } else if (message.content.toLowerCase().startsWith(prefix + commands[1])) {
     const args = message.content.split(" ");
-    media.playStream(ytdl(args[1], {
+    let m = media.get(args[1]);
+    if (!m) {
+      m = new MediaPlayer(false, 5030 + (++currPlayerPort));
+      media.set(args[1], m);
+    }
+    m.playStream(ytdl(args[2], {
       filter: 'audioonly',
       quality: 'highestaudio',
       highWaterMark: 1024*1024*10, // 10mb
@@ -53,17 +68,28 @@ client.on("message", (message) => {
         }
       }
     }, {highWaterMark: 1}));
-    voice.play(media);
+    const connection = voice.getVoiceConnection(args[1]);
+    if (!connection.media) {
+      connection.play(m);
+    }
   } else if (message.content.toLowerCase().startsWith(prefix + commands[2])) {
     message.reply("Pong");
   } else if (message.content.toLowerCase().startsWith(prefix + commands[3])) {
-    media.pause();
+    const args = message.content.split(" ");
+    let m = media.get(args[1]);
+    m.pause();
   } else if (message.content.toLowerCase().startsWith(prefix + commands[4])) {
-    media.resume();
+    const args = message.content.split(" ");
+    let m = media.get(args[1]);
+    m.resume();
   } else if (message.content.toLowerCase().startsWith(prefix + commands[5])) {
-    voice.leave();
+    const args = message.content.split(" ");
+    let connection = voice.getVoiceConnection(args[1]);
+    connection.leave();
   } else if (message.content.toLowerCase().startsWith(prefix + commands[6])) {
-    media.stop();
+    const args = message.content.split(" ");
+    let m = media.get(args[1]);
+    m.stop();
   }
 });
 
