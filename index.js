a113212efdb2013d7ef8a342b65707221e845783
ShadowLp174 <samuel.moravec@yahoo.com>
2022-09-02 21:09:16 +0200

    added user information

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const User = require("./User.js");
 const Media = require("./Media.js");
 const Revoice = require("./Revoice.js");
 const Signaling = require("./Signaling.js");
 
 module.exports = {
-  ...Media, Revoice, Signaling
+  ...Media, Revoice, Signaling, User
 }
