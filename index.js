a36fb50d1c45afc546d72063959a24dea0bd02cb
ShadowLp174 <samuel.moravec@yahoo.com>
2022-10-19 13:08:47 +0100

    init

--- index.js
+++
@@ -1,8 +0,0 @@
-const User = require("./User.js");
-const Media = require("./Media.js");
-const Revoice = require("./Revoice.js");
-const Signaling = require("./Signaling.js");
-
-module.exports = {
-  ...Media, Revoice, Signaling, User
-}
